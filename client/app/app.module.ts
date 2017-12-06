1194a5411bb36a84cfcdd3c7028e59d2cc60e738
Estagiario DTI <estdti@unimedmaceio.local>
2017-12-06 12:03:28 -0300

    segundo modulo

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
-
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { FotoModule } from './foto/foto.module';
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FotoModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule{}
+export class AppModule { }
